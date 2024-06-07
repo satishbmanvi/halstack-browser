f4543b7adbc439334f29695dd3af0e6051528898
s273 <satishb@dxc.com>
2024-06-07 15:13:00 +0530

    Ticket #IBPLIFE-36500

--- lib/src/hal-ui/InputPropertiesList.jsx
+++ lib/src/hal-ui/InputPropertiesList.jsx
@@ -6,17 +6,25 @@ const InputPropertiesList = ({ properties, disabled, propertyUpdateFunction }) =
   <PropertiesListStyled>
     {properties.map((property) => (
       <PropertyLayout>
-        <Label>
-          {property.key}
-          {property.required ? "*" : ""}
-        </Label>
+        <PropertyWrapper>
+          <Label>
+            {property.key}
+            <Text>{property.required == 'true' ? " * required" : ""}</Text>
+          </Label>
+          <div className="inline-labels">
+            <Label2 className="label-italic">{`${property.type}`}</Label2>,
+            <Label3>{` ${property.format !== undefined && property.format !== null && property.format !== '' ? property.format : '-'}`}</Label3>
+          </div>
+          <Label1>{`min:${property.minLength} max:${property.maxLength}`}</Label1>
+        </PropertyWrapper>
         <InputPropertyLayout>
+          <Label>{property.description != undefined && property.description != '' && property.description != null ? property.description : '-'}</Label>
           <InputProperty
             disabled={disabled}
             name={property.key}
-            type={property.metadata.type}
-            format={property.metadata.format}
-            enumOptions={property.metadata.enum}
+            type={property.type}
+            // format={property.metadata.format}
+            // enumOptions={property.metadata.enum}
             required={property.required}
             propertyUpdateFunction={propertyUpdateFunction}
           />
@@ -31,15 +39,39 @@ const PropertiesListStyled = styled.div`
   flex-direction: column;
 `;
 
+const PropertyWrapper = styled.span`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Text = styled.span`
+ color: red;
+ font-size: 13px;
+`;
+
 const PropertyLayout = styled.div`
   display: flex;
   margin: 5px 0px;
 `;
 
-const Label = styled.span`
-  width: 50%;
-  display: inline-flex;
-  align-items: center;
+const Label = styled.div`
+  margin-bottom: 1px;
+`;
+
+const Label1 = styled.div`
+  font-size: 12px;
+  font-family: monospace;
+`;
+
+const Label2 = styled.span`
+  font-size: 12px;
+  font-style: italic;
+  font-weight: 600;
+`;
+
+const Label3 = styled.span`
+font-size: 12px;
 `;
 
 const InputPropertyLayout = styled.div`
